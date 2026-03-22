265acdef9eb722dcd5afb5d799b1af229c105e7b
Arjun Sharma <whocaresaboutarjun@gmail.com>
2026-03-22 18:39:55 +0000

    feat: update the product cards and remove horizontal scroll

--- webshop_wrapper/www/store_home/index.py
+++ webshop_wrapper/www/store_home/index.py
@@ -2,7 +2,14 @@ import frappe
 from frappe import _
 from frappe.query_builder import Order
 from frappe.query_builder import functions as fn
-from frappe.utils import add_days, nowdate
+from frappe.utils import add_days, cint, nowdate
+from frappe.utils.data import flt
+from decimal import Decimal
+from datetime import date, datetime
+
+from webshop.webshop.product_data_engine.query import ProductQuery
+from webshop.webshop.shopping_cart.product_info import get_product_info_for_website
+from webshop_wrapper.variant_defaults import pick_default_variant_item_code
 
 
 sitemap = 1
@@ -16,6 +23,7 @@ WEBSITE_ITEM_FIELDS = [
 	"website_image",
 	"thumbnail",
 	"item_group",
+	"has_variants",
 	"route",
 	"ranking",
 	"modified",
@@ -36,6 +44,49 @@ def get_context(context):
 
 	if homepage and homepage.show_top_10_best_sellers:
 		context.top_sellers = get_top_sellers(limit=10, days=90)
+
+	query_engine = ProductQuery()
+	cart_items = query_engine.get_cart_items() if query_engine.settings.enabled else []
+
+	context.top_sellers = decorate_items_for_cards(context.top_sellers, query_engine, cart_items)
+	context.collections = [
+		{
+			"title": collection["title"],
+			"item_group": collection["item_group"],
+			"item_group_route": collection["item_group_route"],
+			"items": decorate_items_for_cards(collection["items"], query_engine, cart_items),
+		}
+		for collection in context.collections
+	]
+
+	context.sections = []
+	if context.top_sellers:
+		context.sections.append(
+			{
+				"title": _("Top 10"),
+				"item_group_route": "all-products",
+				"cta_label": _("Show More"),
+				"items": context.top_sellers,
+			}
+		)
+
+	for collection in context.collections:
+		context.sections.append(
+			{
+				"title": collection["title"],
+				"item_group_route": collection["item_group_route"],
+				"cta_label": _("Show More"),
+				"items": collection["items"],
+			}
+		)
+
+	context.card_settings = {
+		"enabled": cint(query_engine.settings.enabled),
+		"enable_wishlist": cint(query_engine.settings.enable_wishlist),
+		"show_stock_availability": cint(query_engine.settings.show_stock_availability),
+		"allow_items_not_in_stock": cint(query_engine.settings.allow_items_not_in_stock),
+		"enable_checkout": cint(query_engine.settings.enable_checkout),
+	}
 
 	return context
 
@@ -144,6 +195,69 @@ def normalize_item_images(items):
 	return items
 
 
+def decorate_items_for_cards(items, query_engine, cart_items):
+	if not items:
+		return []
+
+	decorated = []
+	for source_item in items:
+		item = frappe._dict(source_item)
+		item.wished = False
+		item.formatted_price = ""
+		item.formatted_mrp = ""
+		item.discount = ""
+		item.in_stock = 0
+
+		listing_item_code = item.item_code
+		if item.get("has_variants"):
+			default_variant_item_code = pick_default_variant_item_code(item.item_code)
+			if default_variant_item_code:
+				listing_item_code = default_variant_item_code
+				item.item_code = default_variant_item_code
+				item.has_variants = 0
+
+		product_info = get_product_info_for_website(listing_item_code, skip_quotation_creation=True).get(
+			"product_info"
+		)
+
+		if product_info and product_info.get("price"):
+			price = product_info["price"]
+			item.formatted_price = price.get("formatted_price")
+			item.formatted_mrp = price.get("formatted_mrp")
+			if price.get("discount_percent"):
+				item.discount_percent = flt(price.get("discount_percent"))
+			item.discount = price.get("formatted_discount_percent") or price.get("formatted_discount_rate")
+
+		if query_engine.settings.show_stock_availability:
+			item.in_stock = cint(product_info.get("in_stock")) if product_info else 0
+			if product_info and product_info.get("on_backorder"):
+				item.on_backorder = 1
+
+		item.in_cart = listing_item_code in cart_items
+
+		if frappe.session.user != "Guest" and frappe.db.exists(
+			"Wishlist Item", {"item_code": listing_item_code, "parent": frappe.session.user}
+		):
+			item.wished = True
+
+		decorated.append(make_json_safe_item(dict(item)))
+
+	return decorated
+
+
+def make_json_safe_item(item):
+	json_safe = {}
+	for key, value in item.items():
+		if isinstance(value, (datetime, date)):
+			json_safe[key] = value.isoformat()
+		elif isinstance(value, Decimal):
+			json_safe[key] = float(value)
+		else:
+			json_safe[key] = value
+
+	return json_safe
+
+
 def get_top_sellers(limit=10, days=90):
 	from_date = add_days(nowdate(), -days)
 
@@ -164,6 +278,7 @@ def get_top_sellers(limit=10, days=90):
 			website_item.website_image,
 			website_item.thumbnail,
 			website_item.item_group,
+			website_item.has_variants,
 			website_item.route,
 			website_item.ranking,
 			website_item.modified,
@@ -180,6 +295,7 @@ def get_top_sellers(limit=10, days=90):
 			website_item.website_image,
 			website_item.thumbnail,
 			website_item.item_group,
+			website_item.has_variants,
 			website_item.route,
 			website_item.ranking,
 			website_item.modified,
